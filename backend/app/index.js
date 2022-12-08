e8399f43bf72cdbad88bedc4f0f8b85fc3c21802
pf55351 <francesco.primerano79@gmail.com>
2022-12-08 15:39:21 +0100

    fixed bugs added serverless-http

---
+++ backend/app/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const cors = require("cors");
+const router = express.Router();
+
+const message = require("../utils/message");
+const utils = require("../utils/utils");
+
+const myToken = process.env.TOKEN;
+
+/* MIDDLEWARE */
+app.use(cors({ origin: "https://ethereum-verification-address.netlify.app/" }));
+app.use(express.json());
+
+app.use("/", router); // path must route to lambda
+app.use("/.netlify/functions/index", router); // path must route to lambda
+
+router.post(`/verify-address`, function (req, res) {
+  const { ethAddress, token } = { ...req.body };
+  if (token === myToken)
+    utils.isValidAddress(ethAddress).then((data) => res.json(data).status(200));
+  else {
+    const data = { res: "KO", msg: message.ERROR_TOKEN, ethAddress };
+    res.json(data).status(200);
+  }
+});
+
+router.get("/healtcheck", (req, res) => {
+  res.sendStatus(200);
+});
+
+module.exports = app;
+
+module.exports.handler = serverless(app);
--- frontend/src/services/UtilService.js
+++ frontend/src/services/UtilService.js
@@ -1,15 +1,17 @@
-import axios from "axios"
+import axios from "axios";
 
-const serverUrl=process.env.REACT_APP_API_URL
-//const serverPort=process.env.REACT_APP_API_PORT
-const serverContext=process.env.REACT_APP_API_CONTEXT
+const serverUrl = process.env.REACT_APP_API_URL;
+const serverPort = process.env.REACT_APP_API_PORT;
+const serverContext = process.env.REACT_APP_API_CONTEXT;
 
-const serverPath=`${serverUrl}${serverContext}`
+const serverPath = `${serverUrl}${serverPort ? `:${serverPort}` : ""}${
+  serverContext ? serverContext : ""
+}`;
 
-const verifyAddressService = async data => {
-    const endPoint=process.env.REACT_APP_IS_VALID_ADDRESS_SERVICE
-    const result = await axios.post(`${serverPath}${endPoint}`,data);
-    return result.data
-}
+const verifyAddressService = async (data) => {
+  const endPoint = process.env.REACT_APP_IS_VALID_ADDRESS_SERVICE;
+  const result = await axios.post(`${serverPath}${endPoint}`, data);
+  return result.data;
+};
 
-export {verifyAddressService}
+export { verifyAddressService };
